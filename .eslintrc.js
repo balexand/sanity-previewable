cf6af88bd7f6e80464de7ffb31f2c53c5d6bfed3
Brian Alexander <balexand@gmail.com>
2020-12-02 10:56:57 -0700

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
 };
