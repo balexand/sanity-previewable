fc31abb47c28d9bfa7b12699dd9ab1e45b6affaa
Brian Alexander <balexand@gmail.com>
2020-12-02 11:14:34 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const isPublished = ({ _id: _id }) => !_id.startsWith("drafts.");
+
+const sanityFetch = async (client, { params, projection, query }) => {
+  return (await client.fetch([query, projection].join("|"), params)).filter(
+    isPublished
+  );
+};
+
+const applyRequestDefaults = (request) => {
+  return {
+    ...request,
+    params: request.params || {},
+    projection: request.projection || `{ ... }`,
+  };
+};
+
+export const prefetchForDetailPages = async (client, request) => {
+  request = applyRequestDefaults(request);
+
+  return (await sanityFetch(client, request)).map((doc) => {
+    return {
+      params: {
+        ...request.params,
+        __ids: [doc._id, `drafts.${doc._id}`],
+      },
+      prefetchedResults: [doc],
+      query: [`*[_id in $__ids]`, request.projection].join("|"),
+      sanityConfig: {
+        dataset: client.config().dataset,
+        projectId: client.config().projectId,
+      },
+    };
+  });
+};
+
+export const prefetchForListPage = async (client, request) => {
+  request = applyRequestDefaults(request);
+
+  return {
+    params: request.params,
+    prefetchedResults: await sanityFetch(client, request),
+    query: [request.query, request.projection].join("|"),
+    sanityConfig: {
+      dataset: client.config().dataset,
+      projectId: client.config().projectId,
+    },
+  };
+};
